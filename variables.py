708c1d0e2015d4d56a04f351a32ab0ba1dbd3ec1
Nata73gul <natagul.1973@gmail.com>
2024-10-08 17:45:04 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+completed_homework = 12
+hours_spent = 1.5
+сourse_name = 'Pyton,'
+time_for_homework = hours_spent / completed_homework
+print('Курс:', сourse_name,
+      'всего задач:', completed_homework,
+      ',затрачено часов:', hours_spent,
+      ',среднее время выполнения', time_for_homework)
